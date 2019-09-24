5914385d9d08b69269e11e0ffe624ec3a0c5d243
tomis9 <tomis9@buziaczek.pl>
2019-09-24 17:20:05 +0200

    i3 - script for automatic monitor setup

---
+++ i3/monitors.sh
@@ -0,0 +1,15 @@
+export num_monitors=$(xrandr | grep " connected" | wc -l)
+
+# work setting
+if [ $num_monitors != 0 ] 
+then
+    xrandr --output DVI-I-2-1 --auto
+    xrandr --output DVI-I-2-1 --mode 1920x1080
+
+    xrandr --output DVI-I-3-2 --auto
+    xrandr --output DVI-I-3-2 --primary
+    xrandr --output DVI-I-3-2 --mode 1920x1080 
+    xrandr --output DVI-I-3-2 --left-of DVI-I-2-1
+
+    xrandr --output eDP-1 --off
+fi
