45bf17a1ff39776252f826ea6d5ada50eac0f9fd
tomis9 <tomis9@buziaczek.pl>
2019-09-25 13:49:52 +0200

    i3: updated monitors resolution

--- i3/monitors.sh
+++ i3/monitors.sh
@@ -4,11 +4,11 @@ export num_monitors=$(xrandr | grep " connected" | wc -l)
 if [ $num_monitors != 0 ] 
 then
     xrandr --output DVI-I-2-1 --auto
-    xrandr --output DVI-I-2-1 --mode 1920x1080
+    xrandr --output DVI-I-2-1 --mode 1920x1200
 
     xrandr --output DVI-I-3-2 --auto
     xrandr --output DVI-I-3-2 --primary
-    xrandr --output DVI-I-3-2 --mode 1920x1080 
+    xrandr --output DVI-I-3-2 --mode 1920x1200
     xrandr --output DVI-I-3-2 --left-of DVI-I-2-1
 
     xrandr --output eDP-1 --off
