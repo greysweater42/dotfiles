03af77b41951360b56b33a2cf0d6f45a4ed9b75a
tomis9 <tomis9@buziaczek.pl>
2019-09-29 12:53:39 +0200

    i3: minor updates

--- i3/monitors.sh
+++ i3/monitors.sh
@@ -1,8 +1,11 @@
 export num_monitors=$(xrandr | grep " connected" | wc -l)
 
 # work setting
-if [ $num_monitors != 0 ] 
-then
+if [ $num_monitors = 1 ]; then
+    xrandr --output DVI-I-2-1 --off
+    xrandr --output DVI-I-3-2 --off
+    xrandr --output eDP-1 --auto
+else
     xrandr --output DVI-I-2-1 --auto
     xrandr --output DVI-I-2-1 --mode 1920x1200
 
