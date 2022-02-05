0c966f9ef63d6c3e1c56f1db6e7c71011e8960aa
greysweater42 <greysweater42@gmail.com>
2022-02-05 16:23:26 +0100

    i3 restart switches monitors

--- i3/monitors.sh
+++ i3/monitors.sh
@@ -1,29 +1,13 @@
 export num_monitors=$(xrandr | grep " connected" | wc -l)
-export is_home=$(xrandr | grep "HDMI2 connected" | wc -l)
 
 # work setting
 if [ $num_monitors = 1 ]; then
-    xrandr --output DVI-I-2-1 --off
-    xrandr --output DVI-I-3-2 --off
+    xrandr --output HDMI-1 --off
     xrandr --output eDP-1 --auto
-elif [ $is_home == 1 ]; then
-    xrandr --output HDMI2 --auto
-    xrandr --output HDMI2 --mode 1920x1080
-
-    xrandr --output HDMI3 --auto
-    xrandr --output HDMI3 --primary
-    xrandr --output HDMI3 --mode 1920x1080
-    xrandr --output HDMI3 --left-of HDMI2
-
-    xrandr --output eDP1 --off
+    xrandr --output eDP-1 --primary
 else
-    xrandr --output DVI-I-2-1 --auto
-    xrandr --output DVI-I-2-1 --mode 1920x1200
-
-    xrandr --output DVI-I-3-2 --auto
-    xrandr --output DVI-I-3-2 --primary
-    xrandr --output DVI-I-3-2 --mode 1920x1200
-    xrandr --output DVI-I-3-2 --left-of DVI-I-2-1
-
+    xrandr --output HDMI-1 --auto
+    xrandr --output HDMI-1 --primary
     xrandr --output eDP-1 --off
 fi
+
